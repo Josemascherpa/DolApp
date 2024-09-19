e3d6338c66c83bad1d28d3028a980687fcb2355c
Josemascherpa <joseimascherpa@gmail.com>
2024-09-19 10:51:51 -0300

    portrait

--- src/DolApp.tsx
+++ src/DolApp.tsx
@@ -4,17 +4,26 @@ import 'react-native-gesture-handler';
 
 import { ThemeContextProvider } from './context/ThemeContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useEffect } from 'react';
+import Orientation from 'react-native-orientation-locker';
+
 
 
 const queryClient = new QueryClient();
 
+
+
+
 export const DolApp = () => {
+  useEffect( () => {
+    Orientation.lockToPortrait();
+  }, [] );
+  
   return (
     <QueryClientProvider client={ queryClient }>
       <ThemeContextProvider>
-        <DrawerNavigators />        
+        <DrawerNavigators />
       </ThemeContextProvider>
     </QueryClientProvider>
-
   );
 };
